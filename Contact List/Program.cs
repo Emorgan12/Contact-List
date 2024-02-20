8ca3d3fe6705723318be260ed03767b6608bf04a
Emma Craig <Emma.Craig@numatic.co.uk>
2024-02-20 13:02:38 +0000

    nothing is working but it will eventually!!

--- Contact List/Program.cs
+++ Contact List/Program.cs
@@ -1,12 +1,60 @@
-﻿namespace Contact_List
+﻿using System.Net.Http.Headers;
+
+namespace Contact_List
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string[] contact = NewContact();
+            bool exit = false;
+            string[,] AllContacts = new string[,] { };
+            
+            while (!exit)
+            {
+                Console.WriteLine("Create new contact [1], Update a contact[2], Delete a contact [3], or Exit [4]");
+                string input = Console.ReadLine();
+                if (input == "1") 
+                {
+                    int i = 0;
+                    string[] contact = NewContact();
+                    
+                    
 
-            Console.WriteLine(contact[0] + " " + contact[1]);
+                    
+                }
+                else if (input == "2")
+                {
+                    Console.WriteLine("What is the name and number of the contact you want to update? ");
+                    string toFind = Console.ReadLine();
+                    int found;
+                    foreach (string contact in AllContacts)
+                    {
+                        int index = 0;
+                        if (contact == toFind)
+                        {
+                            found = index;
+                            break;
+                        }
+                        else
+                            index++;
+                        Console.WriteLine(index);
+                    }
+                }
+                else if (input == "3")
+                {
+
+                }
+                else if (input == "4")
+                {
+                    exit = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+                    
+            }
+            
             Console.ReadKey();
         }
         static string[] NewContact()
@@ -26,9 +74,33 @@
                 contact.phoneNumber = Console.ReadLine();
                 return contact.phoneNumber;
             }
-            string[] Newcontact = new string[] { contact.Name, contact.phoneNumber };
-            return Newcontact;
+            string[] newContact = { contact.Name, contact.phoneNumber };
+            return newContact;
         }
-        
+        static string[] UpdateContact(Contact contact)
+        {
+            Console.WriteLine("Do you want to update the contact name? (Y/N) ");
+            string YesNoInput = Console.ReadLine();
+            if (YesNoInput == "Y")
+                contact.Name = UpdateName(contact);
+            Console.WriteLine("Do you want to update the contact number? (Y/N) ");
+            YesNoInput = Console.ReadLine();
+            if (YesNoInput == "Y")
+                contact.phoneNumber = UpdateNumber(contact);
+            static string UpdateName(Contact contact)
+            {
+                Console.WriteLine("What is the new name of the contact? ");
+                contact.Name = Console.ReadLine();
+                return contact.Name;
+            }
+            static string UpdateNumber(Contact contact)
+            {
+                Console.WriteLine("What is the new number of the contact? ");
+                contact.phoneNumber = Console.ReadLine();
+                return contact.phoneNumber;
+            }
+            string[] updatedContact = { contact.Name, contact.phoneNumber };
+            return updatedContact;
+        }
     }   
 }
