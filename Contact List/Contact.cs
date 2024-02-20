38c852932e9d25f20bdc52c1181ba474c0cb7c1e
Emma Craig <Emma.Craig@numatic.co.uk>
2024-02-20 11:26:48 +0000

    new contact creation

---
+++ Contact List/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_List
+{
+    internal class Contact
+    {
+        public string? Name { get; set; }
+        public string? phoneNumber { get; set; }
+    }
+}
--- Contact List/Program.cs
+++ Contact List/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] contact = NewContact();
+
+            Console.WriteLine(contact[0] + " " + contact[1]);
+            Console.ReadKey();
         }
-    }
+        static string[] NewContact()
+        {
+            Contact contact = new Contact();
+            ContactName(contact);
+            ContactNumber(contact);
+            static string ContactName(Contact contact)
+            {
+                Console.WriteLine("What is this contact's name? ");
+                contact.Name = Console.ReadLine();
+                return contact.Name;
+            }
+            static string ContactNumber(Contact contact)
+            {
+                Console.WriteLine("What is this contact's phone number? ");
+                contact.phoneNumber = Console.ReadLine();
+                return contact.phoneNumber;
+            }
+            string[] Newcontact = new string[] { contact.Name, contact.phoneNumber };
+            return Newcontact;
+        }
+        
+    }   
 }
