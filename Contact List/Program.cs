0d2dd969a627b1e2eef4da81ff419e0aa6c7f8d6
Emma Craig <Emma.Craig@numatic.co.uk>
2024-02-20 15:46:03 +0000

    IT WORKS

--- Contact List/Program.cs
+++ Contact List/Program.cs
@@ -14,12 +14,12 @@ namespace Contact_List
             {
                 Console.WriteLine("Create new contact [1], Update a contact[2], Delete a contact [3], or Exit [4]");
                 string input = Console.ReadLine();
-                if (input == "1") 
+                if (input == "1")
                 {
                     int i = 0;
                     string[] aContact = NewContact();
                     AllContacts.Add(aContact);
-                    Console.WriteLine(AllContacts);
+
                 }
                 else if (input == "2")
                 {
@@ -33,32 +33,62 @@ namespace Contact_List
                         Console.WriteLine(contact);
                         foreach (string entry in contact)
                         {
-                           
+
                             if (entry == toFind)
                             {
-                                
+
                                 Console.WriteLine("Contact Found");
                                 found = true;
                                 break;
                             }
                             else
                                 index2++;
-                            
+
                         }
-                        //if (found == false)
-                            //index++;
-                        
-                            
-                        
-                        
-                    }
-                    //string[] updatedContact = UpdateContact(AllContacts[index][index2], AllContacts[index][index2+1]);
-                    //Console.WriteLine(AllContacts[index][index2]);
+                        if (found == false)
+                            index++;
+
+
+
+
+                    } 
+                    string[] updatedContact = UpdateContact(AllContacts[index][index2], AllContacts[index][index2 + 1]);
+                    string[] ToUpdate = AllContacts[index];
+                    AllContacts.Remove(ToUpdate);
+                    AllContacts.Add(updatedContact);
                 }
                 else if (input == "3")
                 {
+                    Console.WriteLine("What is the name of the contact you want to delete? ");
+                    string toFind = Console.ReadLine();
+                    int index = 0;
+                    int index2 = 0;
+                    bool found = false;
+                    foreach (string[] contact in AllContacts)
+                    {
+                        Console.WriteLine(contact);
+                        foreach (string entry in contact)
+                        {
 
+                            if (entry == toFind)
+                            {
+
+                                Console.WriteLine("Contact Found... Deleting....");
+                                found = true;
+                                break;
+                            }
+                            else
+                                index2++;
+
+                        }
+                        if (found == false)
+                            index++;
+
+                    }
+                    string[] ToDelete = AllContacts[index];
+                    AllContacts.Remove(ToDelete);
                 }
+
                 else if (input == "4")
                 {
                     exit = true;
@@ -76,32 +106,23 @@ namespace Contact_List
         {
             string contactName = "";
             string contactNumber = "";
-            ContactName(contactName);
-            ContactNumber(contactNumber);
-            static string ContactName(string contactName)
-            {
-                Console.WriteLine("What is this contact's name? ");
-                contactName = Console.ReadLine();
-                return contactName;
-            }
-            static string ContactNumber(string contactNumber)
-            {
-                Console.WriteLine("What is this contact's phone number? ");
-                contactNumber = Console.ReadLine();
-                return contactNumber;
-            }
-            string[] newContact = { contactName, contactNumber };
-            Console.WriteLine(newContact);
+            Console.WriteLine("What is this contact's name? ");
+            contactName = Console.ReadLine();
+            Console.WriteLine("What is this contact's phone number? ");
+            contactNumber = Console.ReadLine();
+            string[] newContact = new string[] {contactName, contactNumber}; 
             return newContact;
         }
         static string[] UpdateContact(string name, string number)
         {
             Console.WriteLine("Do you want to update the contact name? (Y/N) ");
             string YesNoInput = Console.ReadLine();
+            YesNoInput = YesNoInput.ToUpper();
             if (YesNoInput == "Y")
                 name = UpdateName(name);
             Console.WriteLine("Do you want to update the contact number? (Y/N) ");
             YesNoInput = Console.ReadLine();
+            YesNoInput = YesNoInput.ToUpper();
             if (YesNoInput == "Y")
                 number = UpdateNumber(number);
             static string UpdateName(string name)
@@ -116,7 +137,7 @@ namespace Contact_List
                 number = Console.ReadLine();
                 return number;
             }
-            string[] updatedContact = {name,number};
+            string[] updatedContact = new string[] {name,number};
             return updatedContact;
         }
     }   
